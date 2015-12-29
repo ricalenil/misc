b78e6213c8088563b49ee5701aeef28cc2ebde89
Ricky Fletcher <rickyfletcher@gmail.com>
2015-12-29 15:48:09 -0500

    initial

---
+++ rails-template.rb
@@ -0,0 +1,14 @@
+gem "haml-rails"
+gem "bootstrap-sass"
+gem "pry-rails"
+gem "pry-byebug"
+gem "puma"
+gem "redcarpet"
+
+gem_group :test do
+  gem "minitest-rails"
+  end
+
+  environment "config.sass.preferred_syntax = :sass"
+
+  run "rm README.rdoc"
